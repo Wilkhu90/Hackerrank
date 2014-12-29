10688f60204cd61465580dbd33d7c4cbd13f70fd
Sumeet Wilkhu <sumeet.wilkhu@gmail.com>
2014-12-29 18:32:48 +0530

    Create ListComprehension.py

---
+++ ListComprehension.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+X=int(input())
+Y=int(input())
+Z=int(input())
+N=int(input())
+
+
+arr = [[x, y, z] for x in range(X+1) for y in range(Y+1) for z in range(Z+1) if x + y + z != N ]
+
+print(arr)
+    
+    
+    
